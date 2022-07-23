75f4c0f7373a95fab4e4e770521d2983c4b9bf51
wachehi69 <wachehi@hotmail.com>
2022-07-23 11:32:24 +0200

    Secont commit ...

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		String hangry = "yes";
+		
+		if(hangry == "yes") {
+			System.out.println("eat something .......");
+		}
+		else {
+			System.out.println("Do work ......");
+		}
+		
+	}
+
+}
